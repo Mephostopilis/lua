328e1d6901529ed932665eaa7a9c34ad9926c630
emberfarkas <hubing91@163.com>
2017-06-06 14:28:07 +0800

    fix all

--- src/lapi.c
+++ src/lapi.c
@@ -1013,17 +1013,20 @@ LUA_API int lua_load (lua_State *L, lua_Reader reader, void *data,
 
 
 LUA_API int lua_dump (lua_State *L, lua_Writer writer, void *data, int strip) {
-  int status;
-  TValue *o;
-  lua_lock(L);
-  api_checknelems(L, 1);
-  o = L->top - 1;
-  if (isLfunction(o))
-    status = luaU_dump(L, getproto(o), writer, data, strip);
-  else
-    status = 1;
-  lua_unlock(L);
-  return status;
+#ifdef MINI_LUA
+#else
+	int status;
+	TValue *o;
+	lua_lock(L);
+	api_checknelems(L, 1);
+	o = L->top - 1;
+	if (isLfunction(o))
+		status = luaU_dump(L, getproto(o), writer, data, strip);
+	else
+		status = 1;
+	lua_unlock(L);
+	return status;
+#endif // MINI_LUA
 }
 
 
--- src/linit.c
+++ src/linit.c
@@ -39,23 +39,34 @@
 ** these libs are loaded by lua.c and are readily available to any Lua
 ** program
 */
+#ifdef MINI_LUA
+
 static const luaL_Reg loadedlibs[] = {
-  {"_G", luaopen_base},
-  {LUA_LOADLIBNAME, luaopen_package},
-  {LUA_COLIBNAME, luaopen_coroutine},
-  {LUA_TABLIBNAME, luaopen_table},
-  {LUA_IOLIBNAME, luaopen_io},
-  {LUA_OSLIBNAME, luaopen_os},
-  {LUA_STRLIBNAME, luaopen_string},
-  {LUA_MATHLIBNAME, luaopen_math},
-  {LUA_UTF8LIBNAME, luaopen_utf8},
-  {LUA_DBLIBNAME, luaopen_debug},
-#if defined(LUA_COMPAT_BITLIB)
-  {LUA_BITLIBNAME, luaopen_bit32},
-#endif
-  //{LUA_TESTLIBNAME, luaopen_test}, 
-  {NULL, NULL}
+
+	//{LUA_TESTLIBNAME, luaopen_test}, 
+	{ NULL, NULL }
 };
+#else
+static const luaL_Reg loadedlibs[] = {
+	{ "_G", luaopen_base },
+	{ LUA_LOADLIBNAME, luaopen_package },
+	{ LUA_COLIBNAME, luaopen_coroutine },
+	{ LUA_TABLIBNAME, luaopen_table },
+	{ LUA_IOLIBNAME, luaopen_io },
+	{ LUA_OSLIBNAME, luaopen_os },
+	{ LUA_STRLIBNAME, luaopen_string },
+	{ LUA_MATHLIBNAME, luaopen_math },
+	{ LUA_UTF8LIBNAME, luaopen_utf8 },
+	{ LUA_DBLIBNAME, luaopen_debug },
+#if defined(LUA_COMPAT_BITLIB)
+	{ LUA_BITLIBNAME, luaopen_bit32 },
+#endif
+	//{LUA_TESTLIBNAME, luaopen_test}, 
+	{ NULL, NULL }
+};
+#endif // MINI_LUA
+
+
 
 
 LUALIB_API void luaL_openlibs (lua_State *L) {
