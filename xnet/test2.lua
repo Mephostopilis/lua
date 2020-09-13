4e0ba6e8eb5aef8c3d9d794554c236a9ac74123b
mephostopilis <mephostopilis@gmail.com>
2020-09-13 13:05:03 +0800

    fix:test

--- xnet/test2.lua
+++ xnet/test2.lua
@@ -12,12 +12,12 @@ local host = "127.0.0.1"
 
 local function run()
 	network.Startup()
-	local username = "12311"
+	local username = "bing"
 	local password = "Password"
 	local server = "sample1"
-
+	local TI = 10000
+	local count = 0
 	local c = 0 -- client so
-	local subid = 0
 	local t = {
 		OnDisconnected = function(self, id)
 			print("------------")
@@ -52,8 +52,6 @@ local function run()
 		base_info = function(id, ty, requestObj)
 			if ty == "request" then
 				log("[base_info] => %s", table_dump(requestObj))
-				subid = requestObj.info.subid
-				log("[base_info] => subid =  %d", subid)
 			end
 		end,
 		player_funcs = function(id, ty, requestObj)
@@ -75,6 +73,9 @@ local function run()
 		new_name = function(id, ty, requestObj)
 			log("[new_name] => %s", table_dump(requestObj))
 		end,
+		handshake = function(id, ty, responseObj)
+			-- log.error("[handshake] ===> %s", table_dump(responseObj))
+		end,
 		enter = function(id, ty, responseObj)
 			if ty == "response" then
 				log("[enter] ===> %s", table_dump(responseObj))
@@ -82,11 +83,7 @@ local function run()
 				if ok < 0 then
 					print("err:  not send", ok)
 				end
-				local ok = network.Request(id, "create_chat_session", {sid = 0, to = 12})
-				if ok < 0 then
-					print("err:  not send", ok)
-				end
-				local ok = network.Request(id, "match", {sid = subid})
+				local ok = network.Request(id, "create_chat_session", {sid = 0})
 				if ok < 0 then
 					print("err:  not send", ok)
 				end
@@ -109,12 +106,6 @@ local function run()
 		end,
 		fetch_items = function(id, ty, responseObj)
 			log("[fetch_items] ===> %s", table_dump(responseObj))
-		end,
-		create_chat_session = function(id, ty, responseObj)
-			log("[create_chat_session] ===> %s", table_dump(responseObj))
-		end,
-		match = function(id, ty, responseObj)
-			log("[match] ===> %s", table_dump(responseObj))
 		end
 	}
 
